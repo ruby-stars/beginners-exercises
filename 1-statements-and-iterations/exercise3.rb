eb98a120525d93d19ae41e5c6be6dc22d42bc778
Guilherme <ggpasqualino@gmail.com>
2018-09-12 08:00:20 +0200

    Add space before hash closing

--- 1-statements-and-iterations/exercise3.rb
+++ 1-statements-and-iterations/exercise3.rb
@@ -1,4 +1,4 @@
-shopping_list = { lettuce: 1, cucumber: 2, lemons: 6, potatoes: 10, tomatoes: 5, avocados: 2}
+shopping_list = { lettuce: 1, cucumber: 2, lemons: 6, potatoes: 10, tomatoes: 5, avocados: 2 }
 
 puts 'More than 5:'
 shopping_list.each do |vegetable, quantity|
